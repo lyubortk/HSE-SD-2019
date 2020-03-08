c8a1504136f5a0c3bfd4ee16c6bcc702e9c69b3d
Konstantin Lyubort <lyubortk@gmail.com>
2020-03-08 23:59:59 +0300

    prevent git from changing file endings in test files

--- cli/src/test/scala/ru/hse/lyubortk/cli/SpecBase.scala
+++ cli/src/test/scala/ru/hse/lyubortk/cli/SpecBase.scala
@@ -8,9 +8,9 @@ import org.scalatestplus.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 abstract class SpecBase extends AnyFlatSpec with Matchers {
   protected val ResourcesFolder = "src/test/resources"
-  protected val EmptyFile = s"$ResourcesFolder/empty_file"
-  protected val MultiLineFile = s"$ResourcesFolder/multi_line_file"
-  protected val SimpleFile = s"$ResourcesFolder/simple_file"
+  protected val EmptyFile = s"$ResourcesFolder/empty_file.test"
+  protected val MultiLineFile = s"$ResourcesFolder/multi_line_file.test"
+  protected val SimpleFile = s"$ResourcesFolder/simple_file.test"
   protected val NonExistentFile = s"$ResourcesFolder/no_file_here"
-  protected val BinaryFile = s"$ResourcesFolder/binary_file"
+  protected val BinaryFile = s"$ResourcesFolder/binary_file.test"
 }
